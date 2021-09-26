4e14e11d5cbc819d5dd3697121ab464996e90383
duonghungit24 <hungduonng721@gmail.com>
2021-09-27 00:00:17 +0700

    text input

--- AppView.js
+++ AppView.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import Refresh from './src/refreshControl';
-import ListArr from './src/flastList';
 const AppView = () => {
   return (
     <View style={styles.container}>
-      <ListArr />
+      <Refresh />
     </View>
   );
 };
--- src/refreshControl.js
+++ src/refreshControl.js
@@ -1,55 +1,38 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, ScrollView, RefreshControl} from 'react-native';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const Refresh = () => {
-  const [list, setList] = useState([
-    {key: 1, item: 'item1'},
-    {key: 2, item: 'item2'},
-    {key: 3, item: 'item3'},
-    {key: 4, item: 'item4'},
-    {key: 5, item: 'item5'},
-    {key: 6, item: 'item6'},
-    {key: 7, item: 'item7'},
-    {key: 8, item: 'item8'},
-    {key: 9, item: 'item9'},
-    {key: 10, item: 'item10'},
-  ]);
-  const [refresh, setRefresh] = useState(false);
-  const handleReFresh = () => {
-    setRefresh(true);
-    setList([...list, {key: 100, item: 'item 0'}]);
-    setRefresh(false);
-  };
+  const [name, setName] = useState('');
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      style={styles.container}
-      refreshControl={
-        <RefreshControl refreshing={refresh} onRefresh={handleReFresh} />
-      }>
-      {list.map(i => {
-        return (
-          <View key={i.key} style={styles.wrapper}>
-            <Text style={styles.titleText}>{i.item}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Mời bạn nhập họ và tên</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="nhap ho va ten"
+        onChangeText={value => setName(value)}
+        keyboardType="numeric"
+        maxLength={2}
+      />
+      <Text>{name}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  wrapper: {
-    backgroundColor: 'red',
-    padding: 20,
-    marginTop: 5,
+    alignItems: 'center',
   },
   titleText: {
-    color: 'white',
+    color: 'red',
     fontSize: 24,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'red',
+    width: 200,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
   },
 });
 export default Refresh;
